a06f9920c243a2989fc01e4c562ac64167f0c71f
hym2n <chm03131@gmail.com>
2024-06-01 03:33:47 +0900

    GPT API 연동 구현

---
+++ backend/app.js
@@ -0,0 +1,145 @@
+
+
+var express = require('express');
+const app = express();
+const bodyParser = require("body-parser")
+
+const OpenAI = require('openai');
+require('dotenv').config();
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
+
+app.set("view engine",'ejs');
+app.set('views','./views');
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.get("/api/makeProblem", async(req, res) =>{
+    level = req.body.level;
+    problems = [];
+    answers = [];
+    if(level == 0){
+      level_name = 'Beginner';
+    } else if(level == 1){
+        level_name = 'Intermediate';
+    } else if(level == 2){
+        level_name = 'Expert';
+    } else{
+        level_name = 'Average';
+    }
+
+    //뉴스본문 크롤링
+    newsletter = `[파이낸셜뉴스] 올해 4월까지 국세가 지난해 같은 기간 대비 8조원 이상 줄었다. 법인세수가 13조원 가까이 줄어들면서다.
+
+    최상목 경제부총리 겸 기획재정부 장관이 최근 기자간담회에서 "법인세수가 예상보다 덜 걷히고 있다"고 밝힌 것 보다 더 큰 감소 폭이다. 소득세, 부가가치세 등 다른 세목도 법인세수 감소 대체세목 역할을 못하면서 지난해에 이어 올해도'세수펑크' 가능성이 높아지고 있다.`;
+
+    try {
+        const response = await openai.chat.completions.create({
+            messages: [
+                { role: "system", content: "Create five quiz(Must be the prediction of the future.) to guess future economic phenomena in OX format(e.g. ~하였으므로 이번 달 안에 금리가 오를 것이다.), and print it out with an explanation in the following format: '{quiz:['<QUIZ1>', '<QUIZ2>', '<QUIZ3>', '<QUIZ4>', '<QUIZ5>'], correct_answer:['<QUIZ 1 CORRECT ANSWER(just O or X)>', '<QUIZ 2 CORRECT ANSWER(just O or X)>', '<QUIZ 3 CORRECT ANSWER(just O or X)>', '<QUIZ 4 CORRECT ANSWER(just O or X)>', '<QUIZ 5 CORRECT ANSWER(just O or X)>'], commentary:['<QUIZ1 COMMENTARY>', '<QUIZ2 COMMENTARY>', '<QUIZ3 COMMENTARY>', '<QUIZ4 COMMENTARY>', '<QUIZ5 COMMENTARY>']}" },
+                { role: "user", content: newsletter }
+            ],
+            model: "gpt-3.5-turbo",
+            max_tokens: 4000,
+        });
+            result = response.choices[0].message.content;
+            console.log(result);
+            res.send(result);
+        } catch (error) {
+            res.status(500).send(error.message);
+    }
+
+});
+
+
+// //-------------------[api 만들기 예시 example]---------------------
+
+// //get 요청 테스트 api
+// app.get('/getapitest', function(req, res){
+//     // req.query에 받은 키값이 json으로 들어감
+//     console.log(req.query)
+//     result = { //응답
+//         "message" : "123 테스트",
+//         "data" : req.query
+//         };
+//     res.send(JSON.stringify(result));
+//  });
+
+//  //post 요청 테스트 api
+//  app.post('/postapitest', function(req, res){
+//     //req.body에 받은 json 들어감
+//     console.log(req.body);
+//     result = { //응답
+//         "message" : "123 테스트",
+//         "data" : req.body
+//         };
+//     res.send(JSON.stringify(result)); //응답
+//  });
+
+// //---------------------------------------------------
+
+// app.get('/getRecord', function(req, res){
+//     //진단 결과 조회 기능(보호자)
+//     console.log(req.query)
+//     result = { //응답
+//         "message" : "진단 결과",
+//         "data" : req.query
+//         };
+
+//     res.send(JSON.stringify(result));
+// });
+
+// app.post('/gpt', async(req, res)=>{
+//     try {
+//         const response = await openai.chat.completions.create({
+//             messages: [
+//                 { role: "system", content: "Talking nicely like communication with elder people based on the data sent by the user. in Korean" },
+//                 { role: "user", content: req.body }
+//             ],
+//             model: "gpt-3.5-turbo",
+//             max_tokens: 1000,
+//         });
+//             res.send(response.choices[0].message.content);
+//         } catch (error) {
+//             res.status(500).send(error.message);
+//         }
+// });
+
+// app.get('/gptuse', async(req, res)=>{
+//     const prompt = req.query.prompt;
+
+//     if (!prompt) {
+//         res.status(400).send("Please provide a prompt");
+//         return;
+//       }
+//       try {
+//         const response = await openai.chat.completions.create({
+//             messages: [
+//                 { role: "system", content: "Talking nicely like communication with elder people based on the data sent by the user. in Korean" },
+//                 { role: "user", content: prompt }
+//             ],
+//             model: "gpt-3.5-turbo",
+//             max_tokens: 1000,
+//         });
+//             res.send(response.choices[0].message.content);
+//         } catch (error) {
+//             res.status(500).send(error.message);
+//   }
+// });
+
+
+// app.post('/recordRequest', function(req, res){
+//     //fcm 라이브러리로 노인쪽에 대화 시작하기
+// });
+
+
+
+app.listen(3010,function(req, res){
+    console.log("서버가 실행되고 있습니다.");
+});
---
+++ backend/router/mainRouter.js
@@ -0,0 +1,426 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", function(req, res){
+    // res.redirect('');
+});
+
+router.get("/api/makeProblem", (req, res) =>{
+    level = req.body.level;
+    if(level == 0){
+      //gpt에게 문제생성
+    } else if(level == 1){
+
+    } else if(level == 2){
+
+    } else{
+
+    }
+});
+
+
+
+// router.get("/main", function(req, res){
+//   res.render('index',{title:"메인페이지임암튼만들기귀찮음"});
+// })
+
+// router.get("/knock", function(req, res){
+//   res.render('door');
+// })
+
+// router.get("/kim", function(req, res){
+// 	res.render('kim');
+// })
+
+// router.get("/kimkilgu", function(req, res){
+// 	res.render('kimkilgu');
+// })
+
+// router.get("/circle", function(req, res){
+//   res.render('circle');
+// })
+// router.get("/pudding", function(req, res){
+//   res.render('rect1');
+// })
+// router.get("/ball", function(req, res){
+//   res.render('ball');
+// })
+
+// router.get("/circle1", function(req, res){
+//   res.render('circle1');
+// })
+// router.get("/rect1", function(req, res){
+//   res.render('rect1');
+// })
+
+// router.get("/movie", function(req, res){
+//     res.render('movie');
+// })
+// router.get("/todolist", function(req, res){
+//     res.render('todo');
+// })
+// router.get("/snakegame", function(req, res){
+//   res.render('snakegame');
+// })
+// router.get("/strangetranslate", function(req, res){
+//     res.render('strangetranslator');
+// })
+
+// router.get("/colorful", function(req, res){
+//   res.render('color');
+// })
+
+// router.get("/about", function(req, res){
+//     res.send('About page');
+// })
+
+// router.post("/postapi", function(req, res){
+//     let body = req.body;
+//     console.log(body.query);
+//     res.send('Post API');
+// })
+
+// /* 야민정음 번역 api */
+
+// let words1 = ['미','LH','EH','끼','대','댁','댂','댃','댄','댅','댆','댇','댈','댉','댊','댋','댌','댍','댎','댏','댐','댑','댒','댓','댔','댕','댖','댗','댘','댙','댚','댛',
+// '며','멱','멲','멳','면','멵','멶','멷','멸','멹','멺','멻','멼','멽','멾','멿','몀','몁','몂','몃','몄','명','몆','몇','몈','몉','몊','몋',
+// '귀','귁','귂','귃','귄','귅','귆','귇','귈','귉','귊','귋','귌','귍','귎','귏','귐','귑','귒','귓','귔','귕','귖','귗','귘','귙','귚','귛',
+// '파','팍','팎','팏','판','팑','팒','팓','팔','팕','팖','팗','팘','팙','팚','팛','팜','팝','팞','팟','팠','팡','팢','팣','팤','팥','팦','팧',
+// '피','픽','픾','픿','핀','핁','핂','핃','필','핅','핆','핇','핈','핉','핊','핋','핌','핍','핎','핏','핐','핑','핒','핓','핔','핕','핖','핗',
+// '비','빅','빆','빇','빈','빉','빊','빋','빌','빍','빎','빏','빐','빑','빒','빓','빔','빕','빖','빗','빘','빙','빚','빛','빜','빝','빞','빟',
+// '근','유','위','굿',
+// '삐','삑','삒','삓','삔','삕','삖','삗','삘','삙','삚','삛','삜','삝','삞','삟','삠','삡','삢','삣','삤','삥','삦','삧','삨','삩','삪','삫',
+// '포','폭','폮','폯','폰','폱','폲','폳','폴','폵','폶','폷','폸','폹','폺','폻','폼','폽','폾','폿','퐀','퐁','퐂','퐃','퐄','퐅','퐆','퐇',
+// '거','걱','걲','걳','건','걵','걶','걷','걸','걹','걺','걻','걼','걽','걾','걿','검','겁','겂','것','겄','겅','겆','겇','겈','겉','겊','겋',
+// '겨','격','겪','겫','견','겭','겮','겯','결','겱','겲','겳','겴','겵','겶','겷','겸','겹','겺','겻','겼','경','겾','겿','곀','곁','곂','곃',
+// '꺼','공','흫','ㅱ','우','cl','lo','rn','병','배','디','끄','구',
+// '켜','켝','켞','켟','켠','켡','켢','켣','켤','켥','켦','켧','켨','켩','켪','켫','켬','켭','켮','켯','켰','켱','켲','켳','켴','켵','켶','켷',
+// 'ㅂ','앟','넣',
+// '티','틱','틲','틳','틴','틵','틶','틷','틸','틹','틺','틻','틼','틽','틾','틿','팀','팁','팂','팃','팄','팅','팆','팇','팈','팉','팊','팋',
+// '익','든','T','L','또','야','l','백','개','새','포','EU','U','ij','OH','환','응','권','ㅅㅂㄹㅁ',
+// '조조','돌돔','존좋','스시','복복','부부','북북','불붙','뚀','주작','주주','굴국','속삭','고고','뀨','보보',
+// '쁐쁐','ㄱㅂㄹㅌ','프','픅','픆','픇','픈','픉','픊','픋','플','픍','픎','픏','픐','픑','픒','픓','픔','픕','픖','픗','픘','픙','픚','픛','픜','픝','픞','픟'
+// ];
+
+// let words2 = ['lZl','내','태','77l','머','먹','먺','먻','먼','먽','먾','먿','멀','멁','멂','멃','멄','멅','멆','멇','멈','멉','멊','멋','멌','멍','멎','멏','멐','멑','멒','멓',
+// '띠','띡','띢','띣','띤','띥','띦','띧','띨','띩','띪','띫','띬','띭','띮','띯','띰','띱','띲','띳','띴','띵','띶','띷','띸','띹','띺','띻',
+// '커','컥','컦','컧','컨','컩','컪','컫','컬','컭','컮','컯','컰','컱','컲','컳','컴','컵','컶','컷','컸','컹','컺','컻','컼','컽','컾','컿',
+// '과','곽','곾','곿','관','괁','괂','괃','괄','괅','괆','괇','괈','괉','괊','괋','괌','괍','괎','괏','괐','광','괒','괓','괔','괕','괖','괗',
+// '괴','괵','괶','괷','괸','괹','괺','괻','괼','괽','괾','괿','굀','굁','굂','굃','굄','굅','굆','굇','굈','굉','굊','굋','굌','굍','굎','굏',
+// '네','넥','넦','넧','넨','넩','넪','넫','넬','넭','넮','넯','넰','넱','넲','넳','넴','넵','넶','넷','넸','넹','넺','넻','넼','넽','넾','넿',
+// 'ㄹ','윾','읶','긋',
+// '볘','볙','볚','볛','볜','볝','볞','볟','볠','볡','볢','볣','볤','볥','볦','볧','볨','볩','볪','볫','볬','볭','볮','볯','볰','볱','볲','볳',
+// '쪼','쪽','쪾','쪿','쫀','쫁','쫂','쫃','쫄','쫅','쫆','쫇','쫈','쫉','쫊','쫋','쫌','쫍','쫎','쫏','쫐','쫑','쫒','쫓','쫔','쫕','쫖','쫗',
+// '지','직','짂','짃','진','짅','짆','짇','질','짉','짊','짋','짌','짍','짎','짏','짐','집','짒','짓','짔','징','짖','짗','짘','짙','짚','짛',
+// '저','적','젂','젃','전','젅','젆','젇','절','젉','젊','젋','젌','젍','젎','젏','점','접','젒','젓','젔','정','젖','젗','젘','젙','젚','젛',
+// '77ㅓ','끙','홓','딩','윽','d','b','m','ㅹ','IdH','ㅁ','고','ㅋ',
+// '궈','궉','궊','궋','권','궍','궎','궏','궐','궑','궒','궓','궔','궕','궖','궗','궘','궙','궚','궛','궜','궝','궞','궟','궠','궡','궢','궣',
+// '너','왕','봉',
+// '더','덕','덖','덗','던','덙','덚','덛','덜','덝','덞','덟','덠','덡','덢','덣','덤','덥','덦','덧','덨','덩','덪','덫','덬','덭','덮','덯',
+// '의','E','ㅜ','ㄴ','SE','OF','I','뿌','7H','∧H','딜','日J','lJ','y','애','호도','% ','급','섊',
+// '쬬','뚊','쬲','씌','뾲','쀼','쀾','쁉','도도','짞','쮸','뀱','쏶','꾜','구구',"뾰",
+// '붓x4 X','걡','뜨','뜩','뜪','뜫','뜬','뜭','뜮','뜯','뜰','뜱','뜲','뜳','뜴','뜵','뜶','뜷','뜸','뜹','뜺','뜻','뜼','뜽','뜾','뜿','띀','띁','띂','띃'
+// ];
+
+// router.post('/translateAPI2', function(req, res){
+//   let query1 = req.body.query;
+//   console.log();
+//   console.log('Origin Text: ' + query1);
+//   for(let i = 0; i < words1.length; i++){
+//     if(query1.includes(words1[i])){
+//       let regex = new RegExp(`${words1[i]}`, 'gm');
+//       query1 = query1.replace(regex, `{%${i}%}`);
+      
+//     }
+//     if(query1.includes(words2[i])){
+//       let regex = new RegExp(`${words2[i]}`, 'gm');
+//       query1 = query1.replace(regex, `{%${i+words1.length}%}`);
+      
+//     }
+//   }
+//   for(let i = 0; i < words2.length; i++){
+//     if(query1.includes(`{%${i}%}`)){
+//       let regex = new RegExp(`{%${i}%}`, 'gm');
+//       query1 = query1.replace(regex, words2[i]);
+//     }
+//   }
+//   for(let i = words1.length; i < 2*(words1.length); i++){
+//     if(query1.includes(`{%${i}%}`)){
+//       let regex = new RegExp(`{%${i}%}`, 'gm');
+//       query1 = query1.replace(regex, words1[i-words1.length]);
+//     }
+//   }
+
+//   console.log('Converted Text: ' + query1);
+
+//   res.send(query1);
+
+// })
+
+// /*----------------------------
+//         papago api(star)
+// ------------------------------*/
+// var client_id = 'rftew1xkEQX0x4LXYFhE';
+// var client_secret = 'f0EY3J0B36';
+// let ids = ['LWny4rdaLPHYO2EkHgVX','T7yPwwFKVtpuGSX1SA0v','rftew1xkEQX0x4LXYFhE','PKetq7m_ykq6rFvNNF1M','CafS_hWvisnpz5tRH613']
+// let pws = ['Op5NP4DkAT','JGiuYbJ5Ck','f0EY3J0B36','rJpgov6bZh','uow9_wSyC4']
+// let idRotate = 0;
+
+// router.post('/translateAPI1', function (req, res) {
+//     client_id = ids[idRotate];
+//     client_secret = pws[idRotate];
+//     console.log();
+//     console.log("id is now: " + idRotate);
+//     let query = req.body.query;
+
+//    var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
+//    var request = require('request');
+//    var options = {
+//        url: api_url,
+//        form: {'source':'ko', 'target':'ja', 'text':query},
+//        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+//     };
+//     let text;
+//     const promise = new Promise((resolve, reject) => {
+//         request.post(options, function (error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//               let bodyJSON = JSON.parse(body);
+//               console.log("original text: " + options.form.text);
+//               text = bodyJSON.message.result.translatedText;
+//               console.log("translate API response: " + text);
+              
+//             } else {
+//               res.status(response.statusCode).end();
+//               console.log('error = ' + response.statusCode);
+//               text = ('error = ' + response.statusCode);
+//             }
+//             resolve(text);
+//           });
+        
+//     })
+
+//     promise.then((value) => {
+
+//         return new Promise((resolve, reject) => {
+//             options.form.source = 'ja';
+//             options.form.target = 'zh-CN';
+//             options.form.text = value;
+//             request.post(options, function (error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//               let bodyJSON = JSON.parse(body);
+//               console.log("original text: " + options.form.text);
+//               text = bodyJSON.message.result.translatedText;
+//               console.log("translate API response: " + text);
+//               resolve(text);
+//             } else {
+//               res.status(response.statusCode).end();
+//               console.log('error = ' + response.statusCode);
+//               text = ('error = ' + response.statusCode);
+//             }
+//             resolve(text);
+//           });
+//         });
+        
+//     })
+//     .then((value) => {
+
+//         return new Promise((resolve, reject) => {
+//             options.form.source = 'zh-CN';
+//             options.form.target = 'ko';
+//             options.form.text = value;
+//             request.post(options, function (error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//               let bodyJSON = JSON.parse(body);
+//               console.log("original text: " + options.form.text);
+//               text = bodyJSON.message.result.translatedText;
+//               console.log("translate API response: " + text);
+//               resolve(text);
+//             } else {
+//               res.status(response.statusCode).end();
+//               console.log('error = ' + response.statusCode);
+//               text = ('error = ' + response.statusCode);
+//             }
+//             resolve(text);
+//           });
+//         });
+        
+//     })
+//     .then((value) => {
+
+//         return new Promise((resolve, reject) => {
+//             options.form.source = 'ko';
+//             options.form.target = 'fr';
+//             options.form.text = value;
+//             request.post(options, function (error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//               let bodyJSON = JSON.parse(body);
+//               console.log("original text: " + options.form.text);
+//               text = bodyJSON.message.result.translatedText;
+//               console.log("translate API response: " + text);
+//               resolve(text);
+//             } else {
+//               res.status(response.statusCode).end();
+//               console.log('error = ' + response.statusCode);
+//               text = ('error = ' + response.statusCode);
+//             }
+//             resolve(text);
+//           });
+//         });
+        
+//     })
+//     .then((value) => {
+
+//         return new Promise((resolve, reject) => {
+//             options.form.source = 'en';
+//             options.form.target = 'ja';
+//             options.form.text = value;
+//             request.post(options, function (error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//               let bodyJSON = JSON.parse(body);
+//               console.log("original text: " + options.form.text);
+//               text = bodyJSON.message.result.translatedText;
+//               console.log("translate API response: " + text);
+//               resolve(text);
+//             } else {
+//               res.status(response.statusCode).end();
+//               console.log('error = ' + response.statusCode);
+//               text = ('error = ' + response.statusCode);
+//             }
+//             resolve(text);
+//           });
+//         });
+        
+//     })
+//     .then((value) => {
+
+//       return new Promise((resolve, reject) => {
+//           options.form.source = 'ja';
+//           options.form.target = 'ko';
+//           options.form.text = value;
+//           request.post(options, function (error, response, body) {
+//           if (!error && response.statusCode == 200) {
+//             let bodyJSON = JSON.parse(body);
+//             console.log("original text: " + options.form.text);
+//             text = bodyJSON.message.result.translatedText;
+//             console.log("translate API response: " + text);
+//             resolve(text);
+//           } else {
+//             res.status(response.statusCode).end();
+//             console.log('error = ' + response.statusCode);
+//             text = ('error = ' + response.statusCode);
+//           }
+//           resolve(text);
+//         });
+//       });
+      
+//   })
+//     .then((value) => {
+
+//       return new Promise((resolve, reject) => {
+//           options.form.source = 'fr';
+//           options.form.target = 'ko';
+//           options.form.text = value;
+//           request.post(options, function (error, response, body) {
+//           if (!error && response.statusCode == 200) {
+//             let bodyJSON = JSON.parse(body);
+//             console.log("original text: " + options.form.text);
+//             text = bodyJSON.message.result.translatedText;
+//             console.log("translate API response: " + text);
+//             resolve(text);
+//           } else {
+//             res.status(response.statusCode).end();
+//             console.log('error = ' + response.statusCode);
+//             text = ('error = ' + response.statusCode);
+//           }
+//           resolve(text);
+//         });
+//       });
+      
+//   })
+//     .then((value) => {
+
+//         return new Promise((resolve, reject) => {
+//             options.form.source = 'ko';
+//             options.form.target = 'zh-TW';
+//             options.form.text = value;
+//             request.post(options, function (error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//               let bodyJSON = JSON.parse(body);
+//               console.log("original text: " + options.form.text);
+//               text = bodyJSON.message.result.translatedText;
+//               console.log("translate API response: " + text);
+//               resolve(text);
+//             } else {
+//               res.status(response.statusCode).end();
+//               console.log('error = ' + response.statusCode);
+//               text = ('error = ' + response.statusCode);
+//             }
+//             resolve(text);
+//           });
+//         });
+        
+//     })
+
+//     .then((value) => {
+//         options.form.source = 'zh-TW';
+//         options.form.target = 'ko';
+//         options.form.text = value;
+//         request.post(options, function (error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//               res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+//               res.end(body);
+//               let bodyJSON = JSON.parse(body);
+//               console.log("original text: " + options.form.text);
+//               text = bodyJSON.message.result.translatedText;
+//               console.log("translate API response: " + text);
+//               //resolve(text);
+//             } else {
+//               res.status(response.statusCode).end();
+//               console.log('error = ' + response.statusCode);
+//               idRotate++;
+//               if(idRotate > 4) idRotate = 0;
+//             }
+//           });
+//     });
+   
+
+//  });
+
+
+
+
+// //----------------------------------------------------------------
+
+
+
+
+// // router.post('/translateAPI', function (req, res) {
+// //     let query = req.body.query;
+
+// //    var api_url = 'https://openapi.naver.com/v1/papago/n2mt';
+// //    var request = require('request');
+// //    var options = {
+// //        url: api_url,
+// //        form: {'source':'ko', 'target':'en', 'text':query},
+// //        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+// //     };
+// //    request.post(options, function (error, response, body) {
+// //      if (!error && response.statusCode == 200) {
+// //        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+// //        res.end(body);
+// //        let bodyJSON = JSON.parse(body);
+// //        let text = bodyJSON.message.result.translatedText;
+// //        console.log("translate API response: " + text);
+// //      } else {
+// //        res.status(response.statusCode).end();
+// //        console.log('error = ' + response.statusCode);
+// //      }
+// //    });
+
+// //  });
+
+//  /*----------------------------
+//         papago api(end)
+// -------------------------------*/
+
+// module.exports = router;
